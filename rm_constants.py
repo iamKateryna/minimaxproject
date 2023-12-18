28e4fc380e20182010a8814e37047e8cc69a651f
nkateryna <kateryna@spacesense.ai>
2023-12-17 20:21:03 -0500

    refactor: move RMs.txt files

--- rm_constants.py
+++ rm_constants.py
@@ -1,2 +1,2 @@
-MAP_2_RMS = ['envs/grids/reward_machines_m2_a1.txt', 'envs/grids/reward_machines_m2_a2.txt']
-MAP_3_RMS = ['envs/grids/reward_machines_m3_a1.txt', 'envs/grids/reward_machines_m3_a2.txt']
+MAP_2_RMS = ['reward_machines/officeWorld/reward_machines_m2_a1.txt', 'reward_machines/officeWorld/reward_machines_m2_a2.txt']
+MAP_3_RMS = ['reward_machines/officeWorld/reward_machines_m3_a1.txt', 'reward_machines/officeWorld/reward_machines_m3_a2.txt']
