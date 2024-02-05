81055106e7d221ac66a390b6f92a051745e6171f
iamKateryna <kateryna.nekhomiazh@mail.utoronto.ca>
2024-02-05 17:59:36 -0500

    fix: make random agent interface similar to minmax and qlearning

--- rl_agents/random/random_agent.py
+++ rl_agents/random/random_agent.py
@@ -5,7 +5,7 @@ class RandomAgent:
     def __init__(self, action_space):
         self.action_space = action_space
 
-    def get_action(self):
+    def get_action(self, state = None, episode = None):
         return random.choice(range(self.action_space.n))
     
     def name(self):
