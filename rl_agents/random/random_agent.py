47f882c17bd2c872cb7683ceaed88500a46c69b8
iamKateryna <kateryna.nekhomiazh@mail.utoronto.ca>
2024-02-07 16:26:24 -0500

    refactor: add .q_table to avoid rewriting more code

--- rl_agents/random/random_agent.py
+++ rl_agents/random/random_agent.py
@@ -4,6 +4,7 @@ import random
 class RandomAgent:
     def __init__(self, action_space):
         self.action_space = action_space
+        self.q_table = {}
 
     def get_action(self, state = None, episode = None):
         return random.choice(range(self.action_space.n))
