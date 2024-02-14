b5a211094b1b6ea5014bfc96bc2d625f43ad0e4c
iamKateryna <kateryna.nekhomiazh@mail.utoronto.ca>
2024-02-14 16:09:56 -0500

    feature: add rms

--- rm_constants.py
+++ rm_constants.py
@@ -6,7 +6,11 @@ A2_SUFFIX = 'a2'
 MAP_3_RM_2_DIFFERENT_COFFEES = [3, f"{RM_PATH_PREFIX}m3_2_different_coffees_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m3_2_different_coffees_{A2_SUFFIX}.txt"]
 MAP_3_RM = [3, f"{RM_PATH_PREFIX}m3_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m3_{A2_SUFFIX}.txt"]
 MAP_2_RM_ORIGINAL_REWARDS = [2, f"{RM_PATH_PREFIX}m2_original_rewards_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m2_original_rewards_{A2_SUFFIX}.txt"]
+
+MAP_2_RM_COMPLETED_TASK_HIGHER_REWARD_2_DIFFERENT_COFFEES = [2, f"{RM_PATH_PREFIX}m2_completed_task_higher_reward_2_different_coffees_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m2_completed_task_higher_reward_2_different_coffees_{A2_SUFFIX}.txt"] # add second coffee sign
 MAP_2_RM_ORIGINAL_REWARDS_2_DIFFERENT_COFFEES = [2, f"{RM_PATH_PREFIX}m2_original_rewards_2_different_coffees_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m2_original_rewards_2_different_coffees_{A2_SUFFIX}.txt"] # add second coffee sign
+MAP_2_RM_BROKEN_DEC_HIGHER_PENALTY_2_DIFFERENT_COFFEES = [2, f"{RM_PATH_PREFIX}m2_broken_dec_higher_penalty_2_different_coffees_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m2_broken_dec_higher_penalty_2_different_coffees_{A2_SUFFIX}.txt"] # add second coffee sign
+
 MAP_2_RM_HIGHER_PENALTY_DECORATION = [2, f"{RM_PATH_PREFIX}m2_higher_penalty_decoration_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m2_higher_penalty_decoration_{A2_SUFFIX}.txt"]
 MAP_2_RM_HIGHER_PENALTY_LOSS = [2, f"{RM_PATH_PREFIX}m2_higher_penalty_loss_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m2_higher_penalty_loss_{A2_SUFFIX}.txt"]
 MAP_2_RM_NO_PENALTY_DECORATION = [2, f"{RM_PATH_PREFIX}m2_no_penalty_decoration_{A1_SUFFIX}.txt", f"{RM_PATH_PREFIX}m2_no_penalty_decoration_{A2_SUFFIX}.txt"]
