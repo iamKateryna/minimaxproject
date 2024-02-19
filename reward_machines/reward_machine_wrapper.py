65b477c81d0b8bfbe82bcffcbdf48943d29d6085
iamKateryna <kateryna.nekhomiazh@mail.utoronto.ca>
2024-02-19 16:44:12 -0500

    refactor: remove deprecated gamma, optimise minmax/qlearning options

--- reward_machines/reward_machine_wrapper.py
+++ reward_machines/reward_machine_wrapper.py
@@ -3,7 +3,7 @@ import numpy as np
 
     
 class RewardMachineWrapper(BaseParallelWrapper):
-    def __init__(self, env, add_crm, gamma):
+    def __init__(self, env, add_crms):
         """
         RM wrapper
         --------------------
@@ -13,12 +13,12 @@ class RewardMachineWrapper(BaseParallelWrapper):
         --------------------
             - env(RewardMachineEnv): It must be an RM environment
             - add_crm(bool):   if True, it will add a set of counterfactual experiences to info
-            - gamma(float):    Discount factor for the environment
         """
 
         super().__init__(env)
-        self.add_crm = add_crm
+        self.add_crms = add_crms
         self.valid_states = {agent_id: None for agent_id in self.env.id_to_reward_machine}
+        self.agent_ids = self.env.id_to_reward_machine
 
     def get_num_rm_states(self):
         return self.env.num_rm_states
@@ -35,62 +35,22 @@ class RewardMachineWrapper(BaseParallelWrapper):
 
     def step(self, actions, agent_type, episode = None):
         # print(f"WRAPPER: actions -> {actions}, agent_type -> {agent_type}, episode -> {episode}")
-
-
         # executing the action in the environment
         # true_propositions and rm_state are for progress tracking purposes
         reward_machine_observations, reward_machine_rewards, done, info, true_propositions, rm_state = self.env.step(actions, agent_type, episode)
 
-        if self.add_crm:
 
+        for id, agent_id in enumerate(self.agent_ids):
+            print(f"id: {id}, agent_id -> {agent_id}, add_crms -> {self.add_crms}")
 
-            for agent_id in self.env.id_to_reward_machine:
-
-                # print(f"CRM PARAMS for {agent_id}: {self.env.crm_params[agent_id]}")
-                if agent_type == 'qlearning':
-
-                    crm_experience = self._get_crm_experience(agent_id, *self.env.crm_params[agent_id])
-
-                elif agent_type == 'minmax':
-
-                    crm_experience = self._get_crm_experience_minmax(agent_id, *self.env.crm_params[agent_id])
-                
-                else:
-                    raise NotImplementedError(f"CRM updates for {agent_type} are not implemented, available options -> 'minmax' or 'qlearning' ")
-
+            if self.add_crms[id]:
+                crm_experience = self._get_crm_experience(agent_id, *self.env.crm_params[agent_id])
                 info[agent_id]["crm-experience"] = crm_experience
 
-        # true_propositions and rm_state are for progress tracking purposes
         return reward_machine_observations, reward_machine_rewards, done, info, true_propositions, rm_state
     
 
-    def _get_rm_experience(self, agent_id, reward_machine, rm_state_id,
-                            observation, action, next_observation, 
-                            done,true_propositions):
-        
-
-        reward_machine_observation = self.env.get_observation(observation, agent_id, rm_state_id, done)
-
-        next_rm_state_id, reward_machine_reward, reward_machine_done  = reward_machine.step(rm_state_id, true_propositions)
-        done = reward_machine_done or done
-
-        next_reward_machine_observation = self.env.get_observation(next_observation, agent_id, next_rm_state_id, done)
-
-        return (reward_machine_observation, action, reward_machine_reward, next_reward_machine_observation, done), next_rm_state_id
-    
-
-    def _get_rm_experience_minmax(self, agent_id, reward_machine, rm_state_id,
-                            observation, action, other_agent_action, next_observation, 
-                            done,true_propositions):
-        
-        reward_machine_observation = self.env.get_observation(observation, agent_id, rm_state_id, done)
-        next_rm_state_id, reward_machine_reward, reward_machine_done  = reward_machine.step(rm_state_id, true_propositions)
-        done = reward_machine_done or done
-        next_reward_machine_observation = self.env.get_observation(next_observation, agent_id, next_rm_state_id, done)
-        return (reward_machine_observation, action, other_agent_action, reward_machine_reward, next_reward_machine_observation, done), next_rm_state_id
-    
-
-    def _get_crm_experience(self, agent_id, observation, action, next_observation, done, true_propositions):
+    def _get_crm_experience(self, agent_id, observation, actions, next_observation, done, true_propositions):
         """
         Returns a list of counterfactual experiences generated per each RM state.
         Format: [..., (observation, action, reward, next_observation, done), ...]
@@ -102,60 +62,110 @@ class RewardMachineWrapper(BaseParallelWrapper):
 
 
         for rm_state_id in reward_machine.get_states():
-            # print(f"get_crm_experience rm_state_id -> {rm_state_id}, true_propositions -> {true_propositions}")
             experience, next_rm_state = self._get_rm_experience(agent_id, reward_machine, rm_state_id, 
-                                                                observation, action, next_observation, 
+                                                                observation, actions, next_observation, 
                                                                 done, true_propositions)
-            # print(f"Experience: {rm_state_id}")
-            # print(f"Observations: {list(experience[0])}")
-            # print(f"Action: {experience[1]}")
-            # print(f"Reward: {experience[2]}")
-            # print(f"Next State: {list(experience[3])}")
-            # print(f"Done: {experience[4]}")
-                    
+            
             reachable_states.add((reward_machine, next_rm_state))
-
-            # print(f"Current rm state -> true_propositions -> new rm state: {rm_state_id} -> {true_propositions} -> {next_rm_state} ")
-
-            # print(f"Adding crm to experience -> {agents_valid_state is None or (reward_machine, next_rm_state) in agents_valid_state}")
 
             if agents_valid_state is None or (reward_machine, rm_state_id) in agents_valid_state:
                 experiences.append(experience)
-        
-        # print(f"experience -> {experiences}")
-        # print(f"reachable_states -> {reachable_states}")
 
         self.valid_states[agent_id] = reachable_states
 
         return experiences
     
 
-    def _get_crm_experience_minmax(self, agent_id, observation, action, other_agent_action, next_observation, done, true_propositions):
-        """
-        Returns a list of counterfactual experiences generated per each RM state.
-        Format: [..., (observation, action, reward, next_observation, done), ...]
-        """
-        reachable_states, experiences = set(), []
-        agents_valid_state = self.valid_states[agent_id]
-        reward_machine = self.env.id_to_reward_machine[agent_id]
-
-        for rm_state_id in reward_machine.get_states():
-            # print(f"get_crm_experience rm_state_id -> {rm_state_id}, true_propositions -> {true_propositions}")
-            experience, next_rm_state = self._get_rm_experience_minmax(agent_id, reward_machine, rm_state_id, 
-                                                                observation, action, other_agent_action, next_observation, 
-                                                                done, true_propositions)
-            reachable_states.add((reward_machine, next_rm_state))
-
-            # print(f"Current rm state -> true_propositions -> new rm state: {rm_state_id} -> {true_propositions} -> {next_rm_state} ")
-
-            # print(f"Adding crm to experience -> {agents_valid_state is None or (reward_machine, next_rm_state) in agents_valid_state}")
-
-            if agents_valid_state is None or (reward_machine, rm_state_id) in agents_valid_state:
-                experiences.append(experience)
+    def _get_rm_experience(self, agent_id, reward_machine, rm_state_id,
+                            observation, actions, next_observation, 
+                            done,true_propositions):
         
-        # print(f"experience -> {experiences}")
-        # print(f"reachable_states -> {reachable_states}")
 
-        self.valid_states[agent_id] = reachable_states
+        reward_machine_observation = self.env.get_observation(observation, agent_id, rm_state_id, done)
 
-        return experiences
+        next_rm_state_id, reward_machine_reward, reward_machine_done  = reward_machine.step(rm_state_id, true_propositions)
+        done = reward_machine_done or done
+
+        next_reward_machine_observation = self.env.get_observation(next_observation, agent_id, next_rm_state_id, done)
+
+        return (reward_machine_observation, actions, reward_machine_reward, next_reward_machine_observation, done), next_rm_state_id
+    
+
+    # def _get_rm_experience(self, agent_id, reward_machine, rm_state_id,
+    #                         observation, action, next_observation, 
+    #                         done,true_propositions):
+        
+
+    #     reward_machine_observation = self.env.get_observation(observation, agent_id, rm_state_id, done)
+
+    #     next_rm_state_id, reward_machine_reward, reward_machine_done  = reward_machine.step(rm_state_id, true_propositions)
+    #     done = reward_machine_done or done
+
+    #     next_reward_machine_observation = self.env.get_observation(next_observation, agent_id, next_rm_state_id, done)
+
+    #     return (reward_machine_observation, action, reward_machine_reward, next_reward_machine_observation, done), next_rm_state_id
+    
+
+    # def _get_rm_experience_minmax(self, agent_id, reward_machine, rm_state_id,
+    #                         observation, action, other_agent_action, next_observation, 
+    #                         done,true_propositions):
+        
+    #     reward_machine_observation = self.env.get_observation(observation, agent_id, rm_state_id, done)
+    #     next_rm_state_id, reward_machine_reward, reward_machine_done  = reward_machine.step(rm_state_id, true_propositions)
+    #     done = reward_machine_done or done
+    #     next_reward_machine_observation = self.env.get_observation(next_observation, agent_id, next_rm_state_id, done)
+    #     return (reward_machine_observation, action, other_agent_action, reward_machine_reward, next_reward_machine_observation, done), next_rm_state_id
+    
+    
+    # def _get_crm_experience(self, agent_id, observation, action, next_observation, done, true_propositions):
+    #     """
+    #     Returns a list of counterfactual experiences generated per each RM state.
+    #     Format: [..., (observation, action, reward, next_observation, done), ...]
+    #     """
+    #     reachable_states, experiences = set(), []
+
+    #     agents_valid_state = self.valid_states[agent_id]
+    #     reward_machine = self.env.id_to_reward_machine[agent_id]
+
+
+    #     for rm_state_id in reward_machine.get_states():
+    #         experience, next_rm_state = self._get_rm_experience(agent_id, reward_machine, rm_state_id, 
+    #                                                             observation, action, next_observation, 
+    #                                                             done, true_propositions)
+            
+    #         reachable_states.add((reward_machine, next_rm_state))
+
+    #         if agents_valid_state is None or (reward_machine, rm_state_id) in agents_valid_state:
+    #             experiences.append(experience)
+
+    #     self.valid_states[agent_id] = reachable_states
+
+    #     return experiences
+    
+
+    # def _get_crm_experience_minmax(self, agent_id, observation, action, other_agent_action, next_observation, done, true_propositions):
+    #     """
+    #     Returns a list of counterfactual experiences generated per each RM state.
+    #     Format: [..., (observation, action, reward, next_observation, done), ...]
+    #     """
+    #     reachable_states, experiences = set(), []
+
+    #     agents_valid_state = self.valid_states[agent_id]
+    #     reward_machine = self.env.id_to_reward_machine[agent_id]
+
+    #     for rm_state_id in reward_machine.get_states():
+    #         experience, next_rm_state = self._get_rm_experience_minmax(agent_id, reward_machine, rm_state_id, 
+    #                                                             observation, action, other_agent_action, next_observation, 
+    #                                                             done, true_propositions)
+    #         reachable_states.add((reward_machine, next_rm_state))
+
+    #         # print(f"Current rm state -> true_propositions -> new rm state: {rm_state_id} -> {true_propositions} -> {next_rm_state} ")
+
+    #         # print(f"Adding crm to experience -> {agents_valid_state is None or (reward_machine, next_rm_state) in agents_valid_state}")
+
+    #         if agents_valid_state is None or (reward_machine, rm_state_id) in agents_valid_state:
+    #             experiences.append(experience)
+
+    #     self.valid_states[agent_id] = reachable_states
+
+    #     return experiences
